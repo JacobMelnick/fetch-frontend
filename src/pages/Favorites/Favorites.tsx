4b7ddd9005d356af62c1d4419463f6f48e59a68f
Jacob Melnick <jake.r.melnick@gmail.com>
2025-02-11 23:56:40 -0600

    favorites

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -3,13 +3,17 @@ import Layout from "../../components/Layout";
 import { DogService } from "@/api/services/DogService";
 import { Dog } from "@/api/models/Dog";
 import DogCard from "../../components/DogComponents/DogCard";
-import { Grid2 } from "@mui/material";
+import { Box, Container, Grid2, Typography } from "@mui/material";
 import { useAtom } from "jotai";
 import { favoritesAtom } from "@/utils/favoritesAtom";
+import { MatchResultResponse } from "@/api/response/SearchResultResponse";
 
 const Favorites = () => {
   const [favoriteIds] = useAtom(favoritesAtom);
   const [favoriteDogs, setFavoriteDogs] = useState<Dog[]>([]);
+  const [matchedDogs, setMatchedDogs] = useState<Dog[]>([]);
+  const [matchedDogIds, setMatchedDogsIds] = useState<string[]>([]);
+  console.log("matchedDogIds:", matchedDogIds);
   const getFavorites = async () => {
     if (favoriteIds.length) {
       const dogResp = await DogService.getsDogsByIds(favoriteIds);
@@ -21,25 +25,86 @@ const Favorites = () => {
     }
   };
 
+  const getDogMatches = async () => {
+    try {
+      await DogService.fetchDogMatches(favoriteIds).then(
+        (data) => {
+          setMatchedDogsIds(data);
+        }
+        //   {
+        //   favoriteDogs.forEach((favorite) => {
+        //     console.log("data:", data);
+        //     console.log("favorite:", favorite.id);
+        //     if (favorite.id === data.match) {
+        //       console.log("data:", data);
+        //     }
+
+        // }
+      );
+    } catch {}
+  };
+
   useEffect(() => {
     getFavorites();
   }, [favoriteIds]);
 
   return (
     <Layout>
-      <h3>Favorites</h3>
-      <Grid2 direction={"row"} size={4} container spacing={2}>
-        {favoriteDogs.map((dog) => (
-          <DogCard
-            key={dog.id}
-            breed={dog.breed}
-            imageUrl={dog.img}
-            age={dog.age}
-            id={dog.id}
-            name={dog.name}
-          />
-        ))}
-      </Grid2>
+      <Box sx={{ minHeight: "100vh" }}>
+        {favoriteDogs.length <= 0 ? (
+          <Container maxWidth="sm" sx={{ height: "100%" }}>
+            <Box textAlign="center" mt={4}>
+              <Typography variant="h5" gutterBottom>
+                No Dogs Favorited
+              </Typography>
+              <Typography variant="body1" paragraph>
+                Start searching and add your favorite dogs to see your match!
+              </Typography>
+            </Box>
+          </Container>
+        ) : (
+          <>
+            <Box textAlign="left">
+              <Typography variant="h5" gutterBottom>
+                Your Favorites
+              </Typography>
+            </Box>
+            <Grid2 direction={"row"} size={4} container spacing={2}>
+              {favoriteDogs.map((dog) => (
+                <DogCard
+                  key={dog.id}
+                  breed={dog.breed}
+                  imageUrl={dog.img}
+                  age={dog.age}
+                  id={dog.id}
+                  name={dog.name}
+                />
+              ))}
+            </Grid2>
+          </>
+        )}
+        {matchedDogs.length > 0 && (
+          <>
+            <Box textAlign="left" sx={{ py: 3 }}>
+              <Typography variant="h5" gutterBottom>
+                Our Matches
+              </Typography>
+            </Box>
+            <Grid2 direction={"row"} size={4} container spacing={2}>
+              {matchedDogs.map((dog) => (
+                <DogCard
+                  key={dog.id}
+                  breed={dog.breed}
+                  imageUrl={dog.img}
+                  age={dog.age}
+                  id={dog.id}
+                  name={dog.name}
+                />
+              ))}
+            </Grid2>
+          </>
+        )}
+      </Box>
     </Layout>
   );
 };
